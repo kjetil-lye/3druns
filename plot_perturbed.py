a8bff42cad9c23b56cb5144f0248697d08ffc30a
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-03-22 12:40:27 +0100

    added plotting script for perturbed data

---
+++ plot_perturbed.py
@@ -0,0 +1,22 @@
+from numpy import *
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import sys
+import netCDF4
+
+with netCDF4.Dataset(sys.argv[1]) as f:
+    for sample in range(4):
+        d = f.variables['sample_%d_rho' % sample][:,:,:]
+
+        N = d.shape[0]
+
+        for k in range(5):
+            dl = d[int(k*N/5),:,:]
+            x,y = mgrid[0:1:N*1j, 0:1:N*1j]
+            plt.pcolormesh(x,y,dl)
+            plt.colorbar()
+            plt.savefig('img/perturbed_%s_%d_%d.png' % (sys.argv[1].replace('.nc', ''), sample,k))
+            plt.close('all')
+
