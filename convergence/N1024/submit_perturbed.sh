8e15b8421dd16bd8e578f1b3c743e5bf5ebece43
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-03-25 14:16:38 +0200

    added submit scripts

--- convergence/N1024/submit_perturbed.sh
+++ convergence/N1024/submit_perturbed.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kjetil.lye@sam.math.ethz.ch
 #SBATCH --time=24:00:00
-#SBATCH --nodes=256
+#SBATCH --nodes=64
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -13,5 +13,5 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun $HOME/alsvinn/build/alsuqcli/alsuqcli --multi-z 16 --multi-y 16 taylorgreen.xml
+srun $HOME/alsvinn/build/alsuqcli/alsuqcli --multi-z 64  taylorgreen.xml
  
