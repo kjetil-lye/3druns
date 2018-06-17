2f97f1e69b3c3504bcddc179bfec929af2d28fa5
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-06-17 18:05:32 +0200

    Added lower perturbation files

--- averaged/N1024/submit_perturbed.sh
+++ averaged/N1024/submit_perturbed.sh
@@ -3,15 +3,15 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kjetil.lye@sam.math.ethz.ch
 #SBATCH --time=24:00:00
-#SBATCH --nodes=128
+#SBATCH --nodes=512
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
-#SBATCH --account=s665
+#SBATCH --account=s839
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun $HOME/alsvinn/build/alsuqcli/alsuqcli --multi-z 128  taylorgreen.xml
+srun $HOME/alsvinn/build/alsuqcli/alsuqcli --multi-z 128  --multi-y 4 taylorgreen.xml
  
