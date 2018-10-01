3d55c187f59361bc706d3d667bec839bc4001401
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-10-01 14:42:06 +0200

    added tg 512x512x512

--- averaged/N512/sample_0/submit_perturbed.sh
+++ averaged/N512/sample_0/submit_perturbed.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kjetil.lye@sam.math.ethz.ch
 #SBATCH --time=24:00:00
-#SBATCH --nodes=256
+#SBATCH --nodes=1024
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -13,5 +13,5 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun shifter run --mpi kjetilly/alsvinn:git alsuqcli --multi-sample 16 --multi-z 16 taylorgreen.xml
+srun shifter run --mpi kjetilly/alsvinn:git alsuqcli --multi-sample 64 --multi-z 16 taylorgreen.xml
  
