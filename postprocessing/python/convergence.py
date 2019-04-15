15f6bb5d0ff646cac08bcb3649ec460c9bd61b69
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-04-15 09:37:34 +0200

    added single sample convergence

--- postprocessing/python/convergence.py
+++ postprocessing/python/convergence.py
@@ -6,7 +6,36 @@ def load(filename, variable):
     with netCDF4.Dataset(filename) as f:
         return f.variables[variable][:,:,:]
 
+def single_sample_convergence(filename_per_resolution, sample, variable, setup):
+    errors = []
 
+    resolutions = np.array(sorted([r for r in filename_per_resolution.keys()]))
+    d_prev = None
+    for resolution in resolutions:
+        filename =filename_per_resolution[resolution]
+        d = load(filename, "sample_{}_{}".format(sample, variable))
+
+        if resolution > resolutions[0]:
+            error = np.sum(np.abs(d - d_prev)/resolution**3)
+            errors.append(error)
+        d_prev = np.repeat(np.repeat(np.repeat(d, 2, 0), 2, 1), 2, 2)
+        
+        
+
+    p = plt.loglog(resolutions[1:], errors, '-o', basex=2, basey=2,
+                   label='Error {}'.format(statistics_name))
+    poly = np.polyfit(np.log(resolutions[1:]), np.log(errors), 1)
+
+    plt.loglog(resolutions[1:], np.exp(poly[1])*resolutions[1:]**poly[0],
+               '--', label='$\\mathcal{{O}}(N^{{{:.2f}}})$'.format(poly[0]),
+               color=p[0].get_color())
+
+    plt.xlabel("Resolution ($N^3$)")
+    
+    plt.xticks(resolutions, ["${}^{{3}}$".format(r) for r in resolutions])
+    plt.ylabel("Error ($||\\cdot||_{L^1(D)}$")
+    plt.title("Convergence of {variable} (sample {sample}, {setup})".format(variable = variable, sample = sample, setup = setup))
+    plt.legend()
 def statistics_convergence(filename_per_resolution, statistics_name, variable, setup):
     errors = []
 
@@ -38,9 +67,22 @@ def statistics_convergence(filename_per_resolution, statistics_name, variable, s
     plt.title("Convergence of {variable} ({statistic}, {setup})".format(variable = variable, statistic = statistics_name, setup = setup))
     plt.legend()
 
-def plot_single_statistics(filename, statistic):
-    # Need to implement paraview here
-    pass
+def plot_single_sample_convergence(resolutions, basename, sample, variable, setup):
+     filenames = {}
+
+    for r in resolutions:
+        filename = basename.format(resolution = r)
+
+        filenames[r] = filename
+
+    single_sample_convergence(filenames, sample, variable, setup)
+
+    showAndSave("convergence_single_sample_{setup}_{sample}_{variable}".format(setup = setup, sample = sample, variable=variable))
+
+    #for r in resolutions:
+    #    plot_single_statistics(filenames[r], statistic)
+    #    #showAndSave("single_level_{statistic}_{variable}".format(statistic = statistic, variable = variable))
+ 
 
     
 def plot_statistics_convergence(resolutions, basename, statistics, variable, setup):
