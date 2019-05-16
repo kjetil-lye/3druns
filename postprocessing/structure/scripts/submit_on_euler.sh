79db7f1e28a33cb8c0aea64bb3ad6677349dcb82
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-05-16 13:55:33 +0200

    added output images

--- postprocessing/structure/scripts/submit_on_euler.sh
+++ postprocessing/structure/scripts/submit_on_euler.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
-for r in 64 128 256 512; 
+export OMP_NUM_THREADS=1
+for t in 0 1;
 do
-    for p in 0.1 0.01;
+    for x in 64 128 256 512; 
     do
-	cd kh_N${x}_${p}
+	for p in 0.1 0.01;
+	do
+	    cd kh_N${x}_${p}
 	
 
-	bsub ../../build/structure_functions -i /cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_${x}_${p}/kh_1.nc -o structure --samples ${x} --equation euler3 --number-of-h $(( ($x*32)/1024 )) --nx $x --ny $x --nz $x;
+	    bsub -R 'rusage[mem=32000]' -W 120:00 ../../build/structure_functions -i /cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_${x}_${p}/kh_${t}.nc -o structure_t_${t} --samples ${x} --equation euler3 --number-of-h $(( ($x*32)/1024 )) --nx $x --ny $x --nz $x;
 	cd ..;
+	done
     done
 done
