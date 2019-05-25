ec5dc52235e4d30149fa94cca0463a43e2832956
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-05-25 09:09:44 +0200

    Included one point wasserstein in submission script, and now actually passing number of integration points

--- postprocessing/notebooks/submit_wasserstein_on_euler.sh
+++ postprocessing/notebooks/submit_wasserstein_on_euler.sh
@@ -9,10 +9,27 @@ do
 
 	for p in 0.1 0.01;
 	do
-	    bsub -n $N -R "rusage[mem=8000]" mpirun -np 50 python ../python/wasserstein_two_point.py --input_basename "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" --title "Kelvin-Helmholtz \$T=$((2*${t}))\$ \$\\epsilon=$p\$ \$K_I=${int_points}\$"
+	    bsub -n $N -R "rusage[mem=8000]" mpirun -np 50 python \
+		 ../python/wasserstein_two_point.py \
+		 --input_basename \
+		 "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" \
+		 --title "Kelvin-Helmholtz \$T=$((2*${t}))\$ \$\\epsilon=$p\$ \$K_I=${int_points}\$"\
+		 --number_of_integration_points=${int_points}
 	    
 	done
     done
 done
+
+for t in 0 1;
+do
+
+    for p in 0.1 0.01;
+    do
+	bsub -R "rusage[mem=8000]" python \
+	     ../python/compute_wasserstein.py \
+	"/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc"
+    done
+done
+
 
 
