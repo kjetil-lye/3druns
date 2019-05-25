dbe92b1379039b76208aa4b80fcb37172d8c60a2
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-05-25 10:50:03 +0200

    added plotting helpers

---
+++ tools/plot_planes.py
@@ -0,0 +1,56 @@
+import netCDF4
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+import sys
+
+if __name__ == '__main__':
+    inname = sys.argv[1]
+    M = 4
+    fig, axes = plt.subplots(M, 3, sharex='all', sharey='all', figsize=(32,32))
+    
+    with netCDF4.Dataset(inname) as f:
+        
+        N = f.variables['sample_0_rho'].shape[0]
+        x, y = np.mgrid[0:1:N*1j, 0:1:N*1j]
+
+        for j in range(0, M):
+            ax = axes[j,0]
+            d = f.variables['sample_0_rho'][:,:,(j*N)//M]
+
+            im = ax.pcolormesh(x, y, d)
+            try:
+                ax.set_xlabel('$x$')
+                ax.set_ylabel('$y$')
+            except:
+                pass
+            fig.colorbar(im, ax=ax)
+            ax.set_title("Fixing $z={}$".format(j/M))
+            
+            ax = axes[j, 1]
+            d = f.variables['sample_0_rho'][:,(j*N)//M,:]
+
+            im = ax.pcolormesh(x, y, d)
+            try:
+                ax.set_xlabel('$x$')
+                ax.set_ylabel('$y$')
+            except:
+                pass
+            fig.colorbar(im, ax=ax)
+            ax.set_title("Fixing $y={}$".format(j/M))
+
+            ax = axes[j, 2]
+            d = f.variables['sample_0_rho'][(j*N)//M, :, :]
+
+            img = ax.pcolormesh(x, y, d)
+            try:
+                ax.set_xlabel('$x$')
+                ax.set_ylabel('$y$')
+            except:
+                pass
+            fig.colorbar(im, ax=ax)
+            ax.set_title("Fixing $z={}$".format(j/M))
+
+        plt.savefig(inname.replace('.nc', '.png'))
