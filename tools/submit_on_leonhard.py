e9b111925f3dcff3d179afee0d9c82d6f9b81ea8
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-05-29 09:19:36 +0200

    added option to move files after done with simulation

--- tools/submit_on_leonhard.py
+++ tools/submit_on_leonhard.py
@@ -3,13 +3,19 @@ import sys
 import os
 import subprocess
 from pathlib import Path
+import git
 
 if __name__ == '__main__':
+
+    repo = git.Repo(search_parent_directories=True)
+
+
     import argparse
 
     parser = argparse.ArgumentParser(description="""
 Submits the configuration file leonhard. NOTE: Will run in folder of configuration file!
 """)
+
 
 
     parser.add_argument('--alsuqcli_path', type=str, default=str(os.path.join(str(Path.home()), 'alsvinn/build/alsuqcli/alsuqcli')),
@@ -22,6 +28,10 @@ Submits the configuration file leonhard. NOTE: Will run in folder of configurati
                         help='Number of processes to use in the sample direction')
     parser.add_argument('--dry_run', action='store_true',
                         help='Only do a dry run, no actual submission done')
+    parser.add_argument('--move_to', type=str,
+                        help='Moves the results to another folder after simulation is done')
+    parser.add_argument('--base_path', type=str, default=repo.working_dir,
+                        help='Base path of this repository')
 
     args = parser.parse_args()
     configuration_file = args.config
@@ -40,6 +50,9 @@ Submits the configuration file leonhard. NOTE: Will run in folder of configurati
 
     r_command_append = "rusage[ngpus_excl_p=8,mem=8000] span[ptile=8]"
 
+    job_chain = os.path.abspath(configuration_file)
+    job_chain = ''.join(ch for ch in job_chain if ch.isalnum() or ch == '_')
+
     command_to_run = [
 
         'bsub',
@@ -49,6 +62,8 @@ Submits the configuration file leonhard. NOTE: Will run in folder of configurati
         f'{args.wait_time}:00',
         '-n',
         str(total_number_of_nodes),
+        '-J',
+        job_chain,
         'mpirun',
         '-np',
         str(total_number_of_nodes),
@@ -64,9 +79,38 @@ Submits the configuration file leonhard. NOTE: Will run in folder of configurati
         os.path.basename(configuration_file),
     ]
 
+
+
+
+
+
     if args.dry_run:
         command_to_run = ['echo', *command_to_run]
 
     print(f'working_dir={configuration_path}')
     subprocess.run(command_to_run, check = True, cwd = configuration_path)
 
+    if args.move_to is not None:
+        destination_base = os.path.relpath(configuration_path, args.base_path)
+
+        destination_folder = os.path.join(args.move_to, destination_base)
+
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder, exist_ok=True)
+
+        command_to_sync = [
+            'bsub',
+            '-w',
+            f'done({job_chain})',
+            '-W',
+            '120:00',
+            'bash',
+            os.path.join(repo.working_dir, 'tools/move_and_remove.sh'),
+            configuration_path,
+            destination_folder
+        ]
+
+        if args.dry_run:
+            command_to_sync = ['echo', *command_to_sync]
+
+        subprocess.run(command_to_sync, check=True, cwd=configuration_path)
