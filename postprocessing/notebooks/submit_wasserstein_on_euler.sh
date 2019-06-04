8c50362c3114491c4de6142cc77030b2197e1fae
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-04 13:57:33 +0200

    added wasserstein

--- postprocessing/notebooks/submit_wasserstein_on_euler.sh
+++ postprocessing/notebooks/submit_wasserstein_on_euler.sh
@@ -7,9 +7,9 @@ do
     for t in 0 1;
     do
 
-	for p in 0.1 0.01;
+	for p in 0.1;
 	do
-	    bsub -n $N -R "rusage[mem=8000]" mpirun -np 50 python \
+	    bsub -W 48:00 -n $N -R "rusage[mem=8000]" mpirun -np 50 python \
 		 ../python/wasserstein_two_point.py \
 		 --input_basename \
 		 "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" \
@@ -25,7 +25,7 @@ do
 
     for p in 0.1 0.01;
     do
-	bsub -R "rusage[mem=8000]" python \
+	bsub -W 24:00 -R "rusage[mem=8000]" python \
 	     ../python/compute_wasserstein.py \
 	"/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc"
     done
