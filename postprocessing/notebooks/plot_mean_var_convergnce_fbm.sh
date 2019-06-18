7630afdadf7811ad9a73a04afa9d2274c4e3d332
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-06-18 17:15:50 +0200

    fixes

--- postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
@@ -5,7 +5,7 @@ do
     do 
 	for H in 0_5; 
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_1.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$" --starting_resolution 32 --stat ${stat} --variable ${var}; 
+	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_1.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
     done; 
 done
@@ -17,7 +17,7 @@ do
     do 
 	for H in 0_5; 
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.0\$" --starting_resolution 32 --stat ${stat} --variable ${var}; 
+	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.0\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
     done; 
 done
