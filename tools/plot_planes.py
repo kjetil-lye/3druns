e3d2d32732672883685f78b8d9c210b31cba6599
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-06-18 08:26:02 +0200

    made plotting script plot in folder

--- tools/plot_planes.py
+++ tools/plot_planes.py
@@ -9,7 +9,34 @@ import sys
 import os
 import plot_info
 import argparse
+
+import glob
+from xml_tools import set_in_xml, get_xml_node, get_in_xml, read_config
+
+def get_parameters(folder):
+    parameters = {}
+    for xml_file in glob.glob(os.path.join(folder, "*.xml")):
+        try:
+            config = read_config(xml_config)
+
+            dimension = get_xml_node(config, "config.fvm.grid.dimension").split(" ")[0]
+            initial_data_parameters = get_xml_node(config, "config.fvm.initialData.parameters")
+
+            for initial_data_parameter in initial_data_parameters.getElementsByTagName("parameter"):
+                name = get_in_xml(initial_data_parameter, "name")
+                length = int(get_in_xml(initial_data_parameter, "name"))
+        
+                if length == 1:
+                    value = get_in_xml(initial_data_parameter, "value")
+                    parameters[name] = value
+        except:
+            pass
+    return parameters
+
+
+
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description="""
 Plot the NetCDF file
         """)
@@ -19,6 +46,9 @@ Plot the NetCDF file
 
     parser.add_argument('--sample', type=int, default=0,
                         help="Sample number to plot")
+
+    parser.add_argument('--in_same_dir', action='store_true',
+                        help='Output the plot in same directory as the data file')
 
     parser.add_argument('--variable', type=str, default='rho',
                         help="variable to plot (rho, mx, my, mz, E, p, ux, uy, uz)")
@@ -30,18 +60,25 @@ Plot the NetCDF file
     inname = args.input_file
     sample = args.sample
     variable = args.variable
-
-    sample_key = 'sample_{}_{}'.format(sample, variable)
+    
+    if "mean" in inname or "variance" in inname:
+        sample_key = variable
+        if "mean" ini inname:
+            description = "mean"
+        else:
+            description = "variance"
+    else:
+        sample_key = 'sample_{}_{}'.format(sample, variable)
+        descriptoin = f"sample = ${sample}$"
     M = 4
     fig, axes = plt.subplots(M, 3, sharex='all', sharey='all', figsize=(32,32))
-
+    
     with netCDF4.Dataset(inname) as f:
-         
+        os.chdir(os.path.dirname(inname))
+        parameters = get_parameters(".")
         N = f.variables[sample_key].shape[0]
-        fig.suptitle('{}, grid_size=${}^3$, sample=${}$, variable={}'.format(inname, \
-                                                                            N, \
-                                                                            sample,
-                                                                            variable),
+        title_parameters = ", ".join([f"${key} = {parameters[key]}$" for key in parameters.keys()])
+        fig.suptitle(f'{inname}, grid_size=${N}^3$, {description}, variable={variable}\n{title_parameters}',
                      fontsize=30)
         x, y = np.mgrid[0:1:N*1j, 0:1:N*1j]
 
@@ -81,6 +118,6 @@ Plot the NetCDF file
                  pass
             fig.colorbar(im, ax=ax)
             ax.set_title("Fixing $x={}$".format(j/M), fontsize=20)
-
+        parameters_as_str = "_".join([f"{key}_{parameters[key]}" for key parameters.keys()])
         plot_info.savePlot(os.path.splitext(os.path.basename(inname))[0] + "_" + \
                            sample_key + "_resolution_" + str(N))
