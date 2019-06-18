9f89682e3e9200bfa08f2bfce92df4b141ac9634
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-18 20:33:27 +0200

    added starting resolution option

--- postprocessing/python/compute_wasserstein.py
+++ postprocessing/python/compute_wasserstein.py
@@ -1,10 +1,18 @@
 import sys
 sys.path.append('../python')
+import numpy as np
 import convergence
 if __name__ == '__main__':
 
-    resolutions=[64, 128, 256, 512]
+    
+
     basename = sys.argv[1]
+    if len(sys.argv) > 2:
+        starting_resolution = int(sys.argv[2])
+    else:
+        starting_resolution = 64
+
+    resolutions = 2**np.arange(int(np.log2(starting_starting)), 10)
     variable = 'rho'
     setup = basename.format(resolution='res')
 
