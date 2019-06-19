3bbc6cee8347131cbf1c7954838408b443473e95
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-19 23:26:17 +0200

    added 0.1 and 0.75

--- postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
@@ -3,7 +3,7 @@ for stat in mean variance;
 do 
     for var in mx my mz E rho; 
     do 
-	for H in 0_5; 
+	for H in 0_1 0_5 0_75; 
 	do 
 	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_1.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
@@ -15,7 +15,7 @@ for stat in mean variance;
 do 
     for var in mx my mz E rho; 
     do 
-	for H in 0_5; 
+	for H in 0_1 0_5 0_75; 
 	do 
 	    python ../python/compute_mean_variance_convergence.py --input $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.0\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
