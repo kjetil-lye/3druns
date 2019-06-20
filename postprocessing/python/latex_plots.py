bb306c9a818f9c862fbfcb1427075e07ef25e5c4
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-20 09:45:27 +0200

    fixed latex plotting

--- postprocessing/python/latex_plots.py
+++ postprocessing/python/latex_plots.py
@@ -4,7 +4,10 @@ matplotlib.rcParams['savefig.dpi'] = 600
 # see https://stackoverflow.com/a/46262952 (for norm symbol)
 # and https://stackoverflow.com/a/23856968
 matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}'] #for \text command
+matplotlib.rcParams['text.latex.preamble'] = [
+    r'\usepackage{amsmath}',
+    r'\usepackage{amsfonts}',
+    r'\usepackage{amssymb}']
 import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
