29b3885597e99097424309c2abd83e28f2a3de39
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-24 15:38:14 +0200

    fixes

--- postprocessing/notebooks/plot_mean_var_convergnce_fbm_float.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_fbm_float.sh
@@ -5,7 +5,7 @@ do
     do 
 	for H in 0_1 0_5 0_75; 
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/fbm_float/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$ (single precision)" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
+	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/fbm/stats_float/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$ (single precision)" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
     done; 
 done
