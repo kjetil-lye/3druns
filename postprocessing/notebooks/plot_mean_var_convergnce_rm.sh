8831c04114da950bc5bb29dcf6c3728e6f0aa6f6
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-07-02 17:45:14 +0200

    updated mean var plots

--- postprocessing/notebooks/plot_mean_var_convergnce_rm.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_rm.sh
@@ -5,7 +5,7 @@ do
     do 
 	for H in 0_05
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/stats/p${H}/N{resolution}/richtmeyermeshkov_{stat}_0.nc --title "Richtmeyer-Meshkov instability \$\\epsilon=${H//_/.}, T=4\$" --starting_resolution 64 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
+	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/stats/p${H}/N{resolution}/richtmeyermeshkov_{stat}_0.nc --title "Richtmeyer-Meshkov instability \$\\epsilon=${H//_/.}, T=4\$" --starting_resolution 64 --stat ${stat} --variable ${var} --compute_rate; 
 	done; 
     done; 
 done
