ba5baadd8d22efa1ac8155b499da7e813e708d4d
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-07-02 10:57:58 +0200

    added script to plot convergence for rm

--- postprocessing/notebooks/plot_mean_var_convergnce_rm.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_rm.sh
@@ -5,7 +5,8 @@ do
     do 
 	for H in 0_05
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/stats/p${H}/N{resolution}/rm_cuda_{stat}_0.nc --title "Richtmeyer-Meshkov \$\\epsilon=${H//_/.}, T=4\$" --starting_resolution 64 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
+	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/stats/H${H}/N{resolution}/richtmeyermeshkov_{stat}_0.nc --title "Richtmeyer-Meshkov instability \$\\epsilon=${H//_/.}, T=4\$" --starting_resolution 64 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
 	done; 
     done; 
 done
+
