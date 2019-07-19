7ec648561fd8d7ee51640f6bb9975eb18136ca37
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-07-19 15:14:29 +0200

    made a combinet to one file script

---
+++ tools/combine_to_one_file.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 1 09:25 2019
+
+@author: Kjetil Olsen Lye
+
+Computes the mean and var from a file with samples
+"""
+
+import netCDF4
+import numpy as np
+import argparse
+import collections
+import os
+import sys
+import re
+import glob
+
+class MeanVarianceComputer:
+
+    def __init__(self):
+        self.count = 0
+        self.mean = None
+        self.m2 = None
+
+    def update(self, data):
+        if self.mean is None:
+            self.mean = np.zeros_like(data, dtype=np.float64)
+            self.m2 = np.zeros_like(data, dtype=np.float64)
+
+        self.count += 1
+        delta = data - self.mean
+        self.mean += delta / self.count
+
+        delta2 = data - self.mean
+        self.m2 += delta * delta2
+
+    def get_mean(self):
+        return self.mean
+
+    def get_variance(self):
+        return self.m2/self.count
+
+    def __getitem__(self, stat):
+
+        if stat == 'mean':
+            return self.get_mean()
+        elif stat == 'variance':
+            return self.get_variance()
+
+        raise Exception(f'Unknown statistics: {stat}')
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description="""
+Converts the file to teh new file format
+            """)
+
+    parser.add_argument('--input_file', type=str, required=True,
+                        help='Input file')
+
+
+    parser.add_argument('--output_file_base', type=str, required=True,
+                        help='Output file')
+
+    parser.add_argument('--number_of_samples', type=int, default=-1,
+                        help='Number of samples to use. If -1, it will use all available samples')
+
+
+    args = parser.parse_args()
+
+
+    time_match = re.search(r'_(\d+).nc', args.input_file)
+    if time_match:
+        timestep = int(time_match.group(1))
+    else:
+        timestep = 0
+    mean_variance = collections.defaultdict(lambda : MeanVarianceComputer())
+
+    attributes = {}
+    time = 0.0
+    with netCDF4.Dataset(args.output_file,'w', format='NETCDF4_CLASSIC'):
+        xdim = None
+        ydim = None
+        zdim = None
+        tdim = None
+
+
+        for filename in glob.glob(args.input_file):
+
+            with netCDF4.Dataset(filename) as f:
+                for v in f.variables.keys():
+                    if v == 'time':
+
+                        time = f.variables['time'][0]
+                        if tdim is None:
+                            tdim = outf.createDimension('t', 1)
+                            t = outf.createVariable("time", np.float64, ("t",))
+                            t[0] = time
+                    else:
+                        d = f.variables[v]
+                        if xdim is None:
+                            xdim = outf.createDimension("x", d.shape[0])
+                            ydim = outf.createDimension("y", d.shape[1])
+                            zdim = outf.createDimension("z", d.shape[2])
+
+                        newvar = outf.createVariable(variable, d.dtype, ("x", "y", "z"))
+                        newvar[:,:,:] = d[:,:,:]
+
+                for attribute_name in f.ncattrs():
+                    outf.setncattr(f'sample_{sample}_{attribute_name}',
+                        f.getncattr(attribute_name))
+
+
+                outf.setncattr("WORKING_DIR", os.getcwd())
+                outf.setncattr("IMPORTANT_NOTE", """
+        This file was converted with the script tools/combine_to_one_file.py
+        in the 3druns repository. This was done as a postprocessing step
+                    """)
+
+
+                outf.setncattr("COMMAND_RUN_TO_COMBINE", " ".join([
+                            sys.executable,
+                            *sys.argv]))
+
+                with open(sys.argv[0]) as scriptfile:
+                    outf.setncattr("SCRIPTFILE", scriptfile.read())
