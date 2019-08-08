bfd7c17cd067d484a4f394c88a5b01ce82dc25d3
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-08-08 17:21:19 +0200

    added compressible euler module

---
+++ postprocessing/python/compressible_euler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 12:08:54 2019
+
+@author: kjetil
+"""
+
+conserved_variables = [
+        'rho',
+        'mx',
+        'my',
+        'mz',
+        'E'
+        ]
+
+latex_variables = {
+    'rho' : '\\rho',
+    'E'   : 'E',
+    'mx' : 'm_x',
+    'my' : 'm_y',
+    'mz' : 'm_z',
+    'ux' : 'u_x',
+    'uy' : 'u_y',
+    'uz' : 'u_z',
+    'p'  : 'p'
+}
