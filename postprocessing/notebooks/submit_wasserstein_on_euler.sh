01ad22295f3dfd14504992cf4536d16e33f01eb4
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-08-15 14:44:49 +0200

    updated with new integration points

--- postprocessing/notebooks/submit_wasserstein_on_euler.sh
+++ postprocessing/notebooks/submit_wasserstein_on_euler.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 N=50
+set -e 
+for t in 0 1;
+do
+
+    for p in 0.1
+    do
+	bsub -N -B -W 120:00 -R "rusage[mem=16000]" python \
+	     ../python/compute_wasserstein.py \
+	    --input_basename "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" \
+	    --title "Kelvin-Helmholtz \$T=$((2*${t}))\$ \$\\epsilon=$p\$ \$K_I=${int_points}\$" \
+	    --starting_resolution 64
+    done
+done
+
+
+
+#!/bin/bash
+N=50
 
 for int_points in 8 16 32 64
 do
@@ -9,28 +27,14 @@ do
 
 	for p in 0.1;
 	do
-	    bsub -W 48:00 -n $N -R "rusage[mem=8000]" mpirun -np 50 python \
+	    bsub -N -B -W 120:00 -n $N -R "rusage[mem=32000]" mpirun -np 50 python \
 		 ../python/wasserstein_two_point.py \
 		 --input_basename \
 		 "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" \
 		 --title "Kelvin-Helmholtz \$T=$((2*${t}))\$ \$\\epsilon=$p\$ \$K_I=${int_points}\$"\
-		 --number_of_integration_points=${int_points}
+		 --number_of_integration_points=${int_points} 
 	    
 	done
     done
 done
-
-for t in 0 1;
-do
-
-    for p in 0.1 0.01;
-    do
-	bsub -W 24:00 -R "rusage[mem=8000]" python \
-	     ../python/compute_wasserstein.py \
-	    --input_basename "/cluster/project/sam/klye/3druns_kh_tube/kelvinhelmholtz_3d_tube/stats/kelvinhelmholtz_{resolution}_${p}/kh_${t}.nc" \
-	    --title "Kelvin-Helmholtz \$T=$((2*${t}))\$ \$\\epsilon=$p\$ \$K_I=${int_points}\$"
-    done
-done
-
-
 
