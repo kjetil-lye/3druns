3c3fecb85727ab239cbe49d53567e8fe69527c82
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-08-21 08:57:07 +0200

    Aded script to plot mean var for rm short time

---
+++ postprocessing/notebooks/plot_mean_var_convergnce_rm_shorttime.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for stat in mean variance; 
+do 
+    for var in mx my mz E rho; 
+    do 
+	for H in 0_05
+	do 
+	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/short_time/p${H}/N{resolution}/richtmeyermeshkov_{stat}_0.nc --title "Richtmeyer-Meshkov instability \$\\epsilon=${H//_/.}, T=2\$" --starting_resolution 64 --stat ${stat} --variable ${var} --compute_rate; 
+	done; 
+    done; 
+done
+
