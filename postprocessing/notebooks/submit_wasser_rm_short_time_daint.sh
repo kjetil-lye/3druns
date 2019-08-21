84bca36a451c5b8b177bb5a00cc9a3def62059f9
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-08-21 09:14:16 +0200

    added submit scripts

---
+++ postprocessing/notebooks/submit_wasser_rm_short_time_daint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+python ../../tools/submit_command_on_daint.py --name rm_wasser --command "python ../python/compute_wasserstein.py --input_basename $SCRATCH/3druns/richtmeyermeshkov/short_time/p0_05/N{resolution}/rm_cuda_0.nc --starting_resolution 64 --title 'Richtmeyer-Meshkov \$\\epsilon=0.05, T=2\$'"
