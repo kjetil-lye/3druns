c6e7e2e3d9570bfe7969aebd327618e51a58cda7
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-09-17 15:20:07 +0200

    updated fbm mean var plotting script

--- postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
+++ postprocessing/notebooks/plot_mean_var_convergnce_fbm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for stat in mean variance; 
 do 
-    for var in mx my mz E rho; 
+    for var in all mx my mz E rho; 
     do 
 	for H in 0_1 0_5 0_75; 
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_1.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
+	    python ../python/compute_mean_variance_convergence.py --input_basename  /project/s913/klye/3druns_fbm/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_1.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.2\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate --reference_solution; 
 	done; 
     done; 
 done
@@ -13,11 +13,13 @@ done
 
 for stat in mean variance; 
 do 
-    for var in mx my mz E rho; 
+    for var in all mx my mz E rho; 
     do 
 	for H in 0_1 0_5 0_75; 
 	do 
-	    python ../python/compute_mean_variance_convergence.py --input_basename $SCRATCH/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.0\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate; 
+
+
+	    python ../python/compute_mean_variance_convergence.py --input_basename  /project/s913/klye/3druns_fbm/3druns/fbm/stats/H${H}/N{resolution}/fbm_{stat}_0.nc --title "Fractional Brownian motion \$H=${H//_/.}, T=0.0\$" --starting_resolution 32 --stat ${stat} --variable ${var} --not_zoom --compute_rate --reference_solution; 
 	done; 
     done; 
 done
