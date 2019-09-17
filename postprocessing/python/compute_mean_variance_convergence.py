ad74957fe7eeaf5c63f9ff86b31bc3b3e61ffc82
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-09-17 10:52:12 +0200

    added possibility to compute reference solutiona nd all variables

--- postprocessing/python/compute_mean_variance_convergence.py
+++ postprocessing/python/compute_mean_variance_convergence.py
@@ -25,7 +25,8 @@ latex_variables = {
     'ux' : 'u_x',
     'uy' : 'u_y',
     'uz' : 'u_z',
-    'p'  : 'p'
+    'p'  : 'p',
+    'all': 'u'
 }
 
 def resolution_exists(basename, resolution, stat):
@@ -47,28 +48,60 @@ def load_plane(filename, plane, variable):
             plot_info.add_additional_plot_parameters(filename.replace("/", "_") + "_" + attr, f.getncattr(attr))
         return f.variables[f'{variable}'][plane,:,:]
 
-def plot_convergence(basename, title, variable, starting_resolution, stat, zoom, compute_rate):
+def plot_convergence(basename, title, variable, starting_resolution, stat, zoom, compute_rate, reference_solution=False):
+
+    if variable != 'all':
+        variables = [variable]
+    else:
+        variables = ['rho', 'mx', 'my', 'mz', 'E']
+
     resolution = starting_resolution
 
     resolutions = []
     errors = []
+    if reference_solution:
+        max_resolution = starting_resolution
+        while resolution_exists(basename, max_resolution, stat):
+            resolutions.append(max_resolution)
+            max_resolution = 2*max_resolution
+        print(f'max_resolution = {resolutions[-1]}')
+        for resolution in resolutions[:-1]:
+            print(resolution)
+            error = 0.0
+            for plane in range(resolutions[-1]):
+                for variable_local in variables:
+                    data_fine = load_plane(basename.format(resolution=resolutions[-1], stat=stat), plane, variable_local)
+                
+                    factor_plane = resolutions[-1]//resolution
+                    data_coarse = load_plane(basename.format(resolution=resolution, stat=stat), plane//factor_plane, variable_local)
+                
+                    while data_coarse.shape[0] < data_fine.shape[0]:
+                        data_coarse = np.repeat(np.repeat(data_coarse, 2, 0), 2, 1)
+
+                    error += np.sum(abs(data_coarse-data_fine))
+            error /= (resolutions[-1])**3
+
+            errors.append(error)
+        resolutions = np.array(resolutions)[:-1]
+    else:
+        while resolution_exists(basename, resolution,stat) and resolution_exists(basename, 2*resolution,stat):
+            print(resolution)
+            error = 0.0
+            for plane in range(2*resolution):
+                for variable_local in variables:
+                    data_fine = load_plane(basename.format(resolution=2*resolution, stat=stat), plane, variable_local)
+                    data_coarse = np.repeat(np.repeat(load_plane(basename.format(resolution=resolution, stat=stat), plane//2, variable_local), 2, 0), 2, 1)
+
+                    error += np.sum(abs(data_coarse-data_fine))
+            error /= (2*resolution)**3
+
+            errors.append(error)
+            resolutions.append(resolution)
+
+            resolution *= 2
+
     
-    while resolution_exists(basename, resolution,stat) and resolution_exists(basename, 2*resolution,stat):
-        print(resolution)
-        error = 0.0
-        for plane in range(2*resolution):
-            data_fine = load_plane(basename.format(resolution=2*resolution, stat=stat), plane, variable)
-            data_coarse = np.repeat(np.repeat(load_plane(basename.format(resolution=resolution, stat=stat), plane//2, variable), 2, 0), 2, 1)
-
-            error += np.sum(abs(data_coarse-data_fine))
-        error /= (2*resolution)**3
-
-        errors.append(error)
-        resolutions.append(resolution)
-
-        resolution *= 2
-
-    resolutions = 2*np.array(resolutions)
+        resolutions = 2*np.array(resolutions)
 
     min_error = np.min(errors)
     max_error = np.max(errors)
@@ -85,16 +118,25 @@ def plot_convergence(basename, title, variable, starting_resolution, stat, zoom,
         plt.legend()
 
     plt.xlabel('Resolution ($N^3$)')
-    plt.ylabel(f'Error ($\\lVert {latex_stat[stat]}({latex_variables[variable]}^{{N}})-{latex_stat[stat]}({latex_variables[variable]}^{{N/2}})\\rVert_{{L^1(D)}}$)')
+    if reference_solution:
+        plt.ylabel(f'Error ($\\lVert {latex_stat[stat]}({latex_variables[variable]}^{{N}})-{latex_stat[stat]}({latex_variables[variable]}^{{{2*resolutions[-1]}}})\\rVert_{{L^1(D)}}$)')
+    else:
+        plt.ylabel(f'Error ($\\lVert {latex_stat[stat]}({latex_variables[variable]}^{{N}})-{latex_stat[stat]}({latex_variables[variable]}^{{N/2}})\\rVert_{{L^1(D)}}$)')
     plt.xticks(resolutions, [f"${r}^3$" for r in resolutions])
     plt.title(f"Convergence of {stat},\n"
               f"{title}\n"
               f"Variable: ${latex_variables[variable]}$")
+    if reference_solution:
+        plot_info.saveData(f'{stat}_convergence_reference_{title}_{variable}_errors', errors)
+        plot_info.saveData(f'{stat}_convergence_reference_{title}_{variable}_resolutions', resolutions)
 
-    plot_info.saveData(f'{stat}_convergence_{title}_{variable}_errors', errors)
-    plot_info.saveData(f'{stat}_convergence_{title}_{variable}_resolutions', resolutions)
+        plot_info.savePlot(f'{stat}_convergence_reference_{title}_{variable}')
+    else:
+        plot_info.saveData(f'{stat}_convergence_{title}_{variable}_errors', errors)
+        plot_info.saveData(f'{stat}_convergence_{title}_{variable}_resolutions', resolutions)
 
-    plot_info.savePlot(f'{stat}_convergence_{title}_{variable}')
+        plot_info.savePlot(f'{stat}_convergence_{title}_{variable}')
+
 
 if __name__ == '__main__':
 
@@ -126,10 +168,13 @@ Computes the stat convergence
     parser.add_argument('--compute_rate', action='store_true',
                         help='Compute the convergence rate')
 
+
+    parser.add_argument('--reference_solution', action='store_true',
+                        help='Compute convergence against a reference solution')
     
     args = parser.parse_args()
 
 
     plot_info.add_additional_plot_parameters("basename", args.input_basename)
 
-    plot_convergence(args.input_basename, args.title, args.variable, args.starting_resolution, args.stat, not args.not_zoom, args.compute_rate)
+    plot_convergence(args.input_basename, args.title, args.variable, args.starting_resolution, args.stat, not args.not_zoom, args.compute_rate, reference_solution=args.reference_solution)
