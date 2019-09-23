d3fc5ab83077baea7a8ff01c0d9e59b95a69a9b1
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-09-23 09:17:02 +0200

    added submit script for fbm on euler

---
+++ postprocessing/notebooks/submit_wasser_fbm_euler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+## NOTE! We could not fit all datasets at the same time on euler, 
+## Therfore, we copied one dataset (one Hurst index H) from daint to euler,
+## computed the wasserstein distance, then deleting the dataset on euler, 
+## and copy the next one
+
+function submit { 
+    echo bsub -W 120:00 -R 'rusage[mem=32000]' "$@"
+}
+
+
+
+H=$1
+
+input_filename=/cluster/project/sam/klye/H${H//./_}/N{resolution}/fbm_1.nc
+
+# Check that we have all the files
+for resolution in 64 128 256 512
+do
+    filename_at_resolution=${input_filename//'{resolution}'/${resolution}}
+    if [[ ! -f "${filename_at_resolution}" ]]
+    then
+	echo "Missing file: ${filename_at_resolution}"
+	exit 1
+    fi
+done
+submit python ../python/compute_wasserstein.py \
+    --input_basename=${input_filename} \
+    --starting_resolution 64 \
+    --title 'Fractional Brownian motion \$H=${H}, T=0.2\$'  \
+    --reference_solution
+
